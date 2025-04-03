9d53de02c25ff93ad36a2d3bada8a17d705ba239
scuartas <scuartas@fiu.edu>
2025-04-03 19:22:29 -0400

    Updated AppImage

--- devel_cmds.sh
+++ devel_cmds.sh
@@ -3,6 +3,7 @@
 alias \
 	build_app="sudo docker build -t ur16_app ." \
 	clean_build_app="sudo docker build --no-cache -t ur16_app ." \
+	build_image="ARCH=x86_64 ./tools/linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage" \
 	run_ur16_app="xhost +local:root && sudo docker run --rm --network host -e DISPLAY=$DISPLAY  -v $(pwd)/config:/config -v /tmp/.X11-unix:/tmp/.X11-unix -it ur16_app"
 
 
